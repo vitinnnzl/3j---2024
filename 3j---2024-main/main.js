8e8ea65284b96bf852851a4f39b51e0160ce59e2
vitinnnzl <108942584+vitinnnzl@users.noreply.github.com>
2024-06-14 09:46:08 -0300

    Add files via upload

--- 3j---2024-main/main.js
+++ 3j---2024-main/main.js
@@ -1,6 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent = "Ola" ;
+const tempoObjetivo1 =new Date (2006,11,20);
+tempo[1].textContent = tempoObjetivo1 ;
+const tempoObjetivo2 =new Date (20,05,2025);
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
         for (let j = 0 ; j < botoes.length ; j++){
